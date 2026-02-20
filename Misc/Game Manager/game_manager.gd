6fdb2aad9c454cf30b0a5ad115ca1de50448a2c7
KingRJ3 <rilesronn@gmail.com>
2026-02-19 21:33:16 -0600

    Fixing conflicts

--- Misc/Game Manager/game_manager.gd
+++ Misc/Game Manager/game_manager.gd
@@ -19,6 +19,8 @@ const TROLLEY_MAIN_SCENE = preload("res://Frameworks(YourStuff)/TrolleyProblem/t
 const ROCK_SKIP = preload("res://Frameworks(YourStuff)/rock-skpping/ww_game.tscn")
 const TYSON = preload("res://Frameworks(YourStuff)/Connor/main.tscn")
 const FINISH_HIM = preload("res://Frameworks(YourStuff)/Kevin/FinishHim/scenes/game_container.tscn")
+const PONG = preload("res://Frameworks(YourStuff)/Pong;)/scenes/main.tscn") # for second stage
+
 
 var all_games : Array[PackedScene] = [BASEPLATE, TROLLEY_MAIN_SCENE, FINISH_HIM, ROCK_SKIP, VANDALISM_JUDE_, TRICK_TAPE, TYSON]
 ## List of games left to play this stage before time scale increases
